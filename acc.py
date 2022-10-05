39088472fca4a4e2f51356df56a4a16ea32ef026
bayuputpra <bayupp.bp@gmail.com>
2022-10-05 15:38:04 +0700

    first commit

--- acc.py
+++ acc.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score,f1_score, recall_score, precision_score, classification_report
+from sklearn.metrics import accuracy_score
 
 warnings.filterwarnings('ignore')
 df = pd.read_csv('../data_tw.csv')
@@ -35,10 +35,4 @@ y_pred = model.predict(X_test)
 y_pred = y_pred.astype(np.int16)
 
 def acc():
-    print(classification_report(test.score_sentiment,y_pred,target_names=['positif 1','netral 0','negatif -1'],labels=[0, 1, 2]))
-    print("Data Test = ",len(test),'%')
-    print("Data Train = ",len(train),'%')
     print("Akurasi = ",accuracy_score(test.score_sentiment,y_pred))
-    print("F1 = ",f1_score(test.score_sentiment.astype(np.int16), y_pred, average='macro'))
-    print("Precision = ",precision_score(test.score_sentiment, y_pred, average='macro'))
-    print("Recall = ",recall_score(test.score_sentiment, y_pred, average='macro'))
