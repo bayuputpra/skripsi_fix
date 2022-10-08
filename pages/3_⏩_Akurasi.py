b2806b2b3dce33f9de8fbcda9fb755be938b1d1c
bayuputpra <bayupp.bp@gmail.com>
2022-10-08 16:00:08 +0700

    frist commit

--- pages/3_⏩_Akurasi.py
+++ pages/3_⏩_Akurasi.py
@@ -4,44 +4,48 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,classification_report,f1_score,precision_score,recall_score
 import streamlit as st
 
 warnings.filterwarnings('ignore')
 st.title("Perhitungan Akurasi")
-file_csv=st.file_uploader("Unggah File CSV")
-df = pd.read_csv(file_csv)
-df
 
-def analyze(score):
-    if score == "positif" :
-        return 1
-    elif score == "netral" :
-        return 0
-    else:
-        return -1
+try:
+    file_csv=st.file_uploader("Unggah File CSV")
+    df = pd.read_csv(file_csv)
+    df
 
-df['score_sentiment'] = df['sentimen'].apply(analyze)
-df
+    def analyze(score):
+        if score == "positif" :
+            return 1
+        elif score == "netral" :
+            return 0
+        else:
+            return -1
 
-train, test = train_test_split(df, test_size=0.2, random_state=30)
+    df['score_sentiment'] = df['sentimen'].apply(analyze)
+    df
 
-#Feature Extraction
-cv = CountVectorizer()
-X_train = cv.fit_transform(train.text)
-X_test = cv.transform(test.text)
+    train, test = train_test_split(df, test_size=0.2, random_state=30)
 
-#model
-model = MultinomialNB()
-model.fit(X_train, train.score_sentiment)
-y_pred = model.predict(X_test)
-y_pred = y_pred.astype(np.int16)
+    #Feature Extraction
+    cv = CountVectorizer()
+    X_train = cv.fit_transform(train.text)
+    X_test = cv.transform(test.text)
 
+    #model
+    model = MultinomialNB()
+    model.fit(X_train, train.score_sentiment)
+    y_pred = model.predict(X_test)
+    y_pred = y_pred.astype(np.int16)
 
-st.text(classification_report(test.score_sentiment,y_pred,target_names=['positif 1','netral 0','negatif -1'],labels=[0, 1, 2]))
-st.text('Data Testing : ',len(test),'%')
-st.text('Data Training : ',len(train),'%')
-st.text('Accuracy : ',accuracy_score(test.score_sentiment,y_pred))
-st.text('F1 Score : ',f1_score(test.score_sentiment.astype(np.int16), y_pred, average='macro'))
-st.text('Precision : ',precision_score(test.score_sentiment, y_pred, average='macro'))
-st.text('Recall : ',recall_score(test.score_sentiment, y_pred, average='macro'))
+    st.text("Hasil Akurasi")
+    st.text(classification_report(test.score_sentiment,y_pred,target_names=['positif 1','netral 0','negatif -1'],labels=[0, 1, 2]))
+    st.text('Data Testing : ',len(test),'%')
+    st.text('Data Training : ',len(train),'%')
+    st.text('Accuracy : ',accuracy_score(test.score_sentiment,y_pred))
+    st.text('F1 Score : ',f1_score(test.score_sentiment.astype(np.int16), y_pred, average='macro'))
+    st.text('Precision : ',precision_score(test.score_sentiment, y_pred, average='macro'))
+    st.text('Recall : ',recall_score(test.score_sentiment, y_pred, average='macro'))
+except:
+    print("error")
