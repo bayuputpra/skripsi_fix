35e28058aea9a05f094ca28654ab041a4ce94261
bayuputpra <bayupp.bp@gmail.com>
2022-10-08 18:50:27 +0700

    first commit

--- 1_⏺_Beranda.py
+++ 1_⏺_Beranda.py
@@ -1,16 +1,8 @@
-from requests import options
 import streamlit as st
-from streamlit_option_menu import option_menu
 
-with st.sidebar:
-	selected=option_menu(
-		menu_title="Main Menu",
-		options=["Beranda","Sentimen","Akurasi"],
-		icons=["house","emoticon","accuracy"],
-		menu_icon="cast",
-		default_index=0,
-		page_title="Skripsi",
-	)
+st.set_page_config(
+	page_title="Skripsi",
+)
 st.title("Judul Skripsi")
 st.image("logo.png")
 st.sidebar.success("Pilih Halaman Diatas")
