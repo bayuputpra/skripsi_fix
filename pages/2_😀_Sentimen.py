df4dd73a80f4f3b92445cb781700b0a1bd361bfa
bayuputpra <bayupp.bp@gmail.com>
2022-10-11 10:26:03 +0700

    first commit

--- pages/2_😀_Sentimen.py
+++ pages/2_😀_Sentimen.py
@@ -19,12 +19,12 @@ auth.set_access_token(access_token,access_token_secret)
 api = tweepy.API(auth)
 
 def main():
-    #st.title("Sentimen Analisis Twitter") 
+    st.title("Sentimen Analisis Twitter") 
 
-    #try:
+    try:
         searchvalue = st.text_input("Masukan Topik Pembahasan Yang Dicari")
-        searchcount = st.slider("Masukan Jumlah Baris Yang Dicari",max_value=1000)
-        hasilSearch  = api.search_tweets(q=searchvalue, count = searchcount, lang='id')
+        searchcount = st.text_input("Masukan Jumlah Baris Yang Dicari")
+        hasilSearch  = api.search_tweets(q=searchvalue, count = int(searchcount), lang='id')
 
         hasilAnalisis = pd.DataFrame(columns=["tgl","user","text"])
 
@@ -126,8 +126,8 @@ def main():
             plt.show()
             st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot(PlotWordcloud())
-    #except:
-        #print("error")
+    except:
+        print("error")
 
 if __name__=='__main__':
         main()
