afb73180b8ceb7890591c9961472362b8ec3a25c
bayuputpra <bayupp.bp@gmail.com>
2022-10-21 23:44:09 +0700

    first commit

--- pages/3_🎡_Akurasi.py
+++ pages/3_🎡_Akurasi.py
@@ -12,27 +12,27 @@ warnings.filterwarnings('ignore')
 st.sidebar.success("Pilih Halaman Diatas")
 st.title("Perhitungan Akurasi")
 
-try:
-    file_csv=st.file_uploader("Unggah File CSV")
-    data = pd.read_csv(file_csv)
-    #remove kolom id
-    st.write(data.iloc[:,1:-1])
+#try:
+file_csv=st.file_uploader("Unggah File CSV")
+data = pd.read_csv(file_csv)
+#remove kolom id
+st.write(data.iloc[:,1:-1])
 
-    label_encoder = LabelEncoder()
-    #mengubah value diagnosis menjadi 1 dan 0 
-    data.iloc[:,0] = label_encoder.fit_transform(data.iloc[:,0]).astype('float64')
+label_encoder = LabelEncoder()
+#mengubah value diagnosis menjadi 1 dan 0 
+data.iloc[:,0] = label_encoder.fit_transform(data.iloc[:,0]).astype('float64')
 
-    paramater = data.iloc[:,1:-1] 
-    target = data.iloc[:,0]
+paramater = data.iloc[:,1:-1] 
+target = data.iloc[:,0]
 
-    x_train, x_test, y_train, y_test = train_test_split(paramater.values, target.values, test_size = 0.2)
+x_train, x_test, y_train, y_test = train_test_split(paramater.values, target.values, test_size = 0.2)
 
-    # The default kernel adalah gaussian kernel
-    svc = SVC() 
-    svc.fit(x_train, y_train) 
-    prediction = svc.predict(x_test)
+# The default kernel adalah gaussian kernel
+svc = SVC() 
+svc.fit(x_train, y_train) 
+prediction = svc.predict(x_test)
 
-    st.text('Model Report :\n'+classification_report(y_test, prediction,target_names=['positif','netral','negatif'],labels=[0,1,2]))
-    st.write("Akurasi : ",metrics.accuracy_score(y_test, prediction))
-except:
-    print("error")
+st.text('Model Report :\n'+classification_report(y_test, prediction,target_names=['positif','netral','negatif'],labels=[0,1,2]))
+st.write("Akurasi : ",metrics.accuracy_score(y_test, prediction))
+#except:
+   #print("error")
