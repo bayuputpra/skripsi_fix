8a720e006d02bdc570de4833e233b0752027631c
bayuputpra <bayupp.bp@gmail.com>
2022-10-23 02:32:56 +0700

    first commit

--- pages/3_🎡_Akurasi.py
+++ pages/3_🎡_Akurasi.py
@@ -12,39 +12,36 @@ st.sidebar.success("Pilih Halaman Diatas")
 st.title("Perhitungan Akurasi")
 
 try:
-    file_csv=st.file_uploader("Unggah File CSV")
-    df = pd.read_csv(file_csv)
-    st.write(df.loc[:,["tgl","user","text","polarity_score","sentimen"]])
+   file_csv=st.file_uploader("Unggah File CSV")
+   df = pd.read_csv(file_csv,sep=";")
+   st.write(df.loc[:,["tgl","user","text","text_clear","polarity_score","sentimen"]])
 
-    def analyze(score):
-        if score == "positif" :
-            return 1
-        elif score == "netral" :
-            return 0
-        else:
-            return -1
+   def analyze(score):
+      if score == "positif" :
+         return 1
+      elif score == "netral" :
+         return 0
+      else:
+         return -1
 
-    df['score_sentiment'] = df['sentimen'].apply(analyze)
+   df['score_sentiment'] = df['sentimen'].apply(analyze)
 
-    train, test = train_test_split(df, test_size=0.8, random_state=42)
+   X=df['text_clear']
+   y=df['score_sentiment']
 
-    #Feature Extraction
-    cv = CountVectorizer()
-    X_train = cv.fit_transform(train.text)
-    X_test = cv.transform(test.text)
+   X_train, X_test, y_train,y_test = train_test_split(X,y, test_size=0.2)
 
-    #model
-    model = MultinomialNB()
-    model.fit(X_train, train.score_sentiment)
-    y_pred = model.predict(X_test)
-    y_pred = y_pred.astype(np.int16)
+   #model
+   model = MultinomialNB()
+   model.fit(X_train, y_train)
+   pred = model.predict(X_test)
 
-    st.text('Model Report :\n'+classification_report(test.score_sentiment,y_pred,target_names=['positif','netral','negatif'],labels=[0,1,2]))
-    st.write('Data Testing : ',len(test),'%')
-    st.write('Data Training : ',len(train),'%')
-    st.write('Accuracy : ',accuracy_score(test.score_sentiment,y_pred))
-    st.write('F1 Score : ',f1_score(test.score_sentiment.astype(np.int16), y_pred, average='macro'))
-    st.write('Precision : ',precision_score(test.score_sentiment, y_pred, average='macro'))
-    st.write('Recall : ',recall_score(test.score_sentiment, y_pred, average='macro'))
+   st.text('Model Report :\n'+classification_report(y_test,pred,target_names=['positif','netral','negatif'],labels=[0,1,2]))
+   #st.write('Data Testing : ',len(test),'%')
+   #st.write('Data Training : ',len(train),'%')
+   st.write('Accuracy : ',accuracy_score(y_test,pred))
+   #st.write('F1 Score : ',f1_score(test.score_sentiment.astype(np.int16), y_pred, average='macro'))
+   #st.write('Precision : ',precision_score(test.score_sentiment, y_pred, average='macro'))
+   #st.write('Recall : ',recall_score(test.score_sentiment, y_pred, average='macro'))
 except:
-    print("error")
+   print("error")
