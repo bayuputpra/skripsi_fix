4784fd90d6ba90bd1639803f0731ef7ef162f21c
bayuputpra <bayupp.bp@gmail.com>
2022-10-23 00:38:37 +0700

    first commit

--- pages/3_🎡_Akurasi.py
+++ pages/3_🎡_Akurasi.py
@@ -1,35 +1,50 @@
+import warnings
+import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score,classification_report
+from sklearn.metrics import accuracy_score,classification_report,f1_score,precision_score,recall_score
 import streamlit as st
 
+warnings.filterwarnings('ignore')
 st.sidebar.success("Pilih Halaman Diatas")
 st.title("Perhitungan Akurasi")
 
-#try:
-file_csv=st.file_uploader("Unggah File CSV")
-#impor CSV ke dataset
-df = pd.read_csv(file_csv)
+try:
+    file_csv=st.file_uploader("Unggah File CSV")
+    df = pd.read_csv(file_csv)
+    st.write(df.loc[:,["tgl","user","text","polarity_score","sentimen"]])
 
-st.write(df.loc[:,["tgl","user","text","polarity_score","sentimen"]])
+    def analyze(score):
+        if score == "positif" :
+            return 1
+        elif score == "netral" :
+            return 0
+        else:
+            return -1
 
-df=df.astype({'sentimen' : 'category'})
-df=df.astype({'text' : 'string'})
-tf=TfidfVectorizer()
-text_tf=tf.fit_transform(df['text'].astype('U'))
+    df['score_sentiment'] = df['sentimen'].apply(analyze)
 
-X_train, X_test, y_train, y_test = train_test_split(text_tf,df['sentimen'], test_size = 0.6, random_state=42)
- 
-clf=MultinomialNB().fit(X_train,y_train)
-predicted=clf.predict(X_test)
-st.write("Accuracy   : ",accuracy_score(y_test,predicted))
-st.write("Precision  : ",precision_score(y_test,predicted,average="macro"))
-st.write("Recall     : ",recall_score(y_test,predicted,average="macro"))
-st.write("F1 Score   : ",f1_score(y_test,predicted,average="macro"))
+    train, test = train_test_split(df, test_size=0.8, random_state=42)
 
-st.text('Model Report :\n'+classification_report(y_test,predicted,target_names=['positif','netral','negatif'],labels=[0,1,2]))
-#except:
-   #print("error")
+    #Feature Extraction
+    cv = CountVectorizer()
+    X_train = cv.fit_transform(train.text)
+    X_test = cv.transform(test.text)
+
+    #model
+    model = MultinomialNB()
+    model.fit(X_train, train.score_sentiment)
+    y_pred = model.predict(X_test)
+    y_pred = y_pred.astype(np.int16)
+
+    st.text('Model Report :\n'+classification_report(test.score_sentiment,y_pred,target_names=['positif','netral','negatif'],labels=[0,1,2]))
+    st.write('Data Testing : ',len(test),'%')
+    st.write('Data Training : ',len(train),'%')
+    st.write('Accuracy : ',accuracy_score(test.score_sentiment,y_pred))
+    st.write('F1 Score : ',f1_score(test.score_sentiment.astype(np.int16), y_pred, average='macro'))
+    st.write('Precision : ',precision_score(test.score_sentiment, y_pred, average='macro'))
+    st.write('Recall : ',recall_score(test.score_sentiment, y_pred, average='macro'))
+except:
+    print("error")
