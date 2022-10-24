db5794c55821c6c6fc6af8962c7d43d7f4b729a7
bayuputpra <bayupp.bp@gmail.com>
2022-10-24 22:29:14 +0700

    first commit

--- pages/3_🎡_Akurasi.py
+++ pages/3_🎡_Akurasi.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
-#from sklearn.naive_bayes import MultinomialNB
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score,classification_report,f1_score,precision_score,recall_score
 import streamlit as st
 
@@ -12,7 +11,7 @@ st.title("Perhitungan Akurasi")
 
 try:
    file_csv=st.file_uploader("Unggah File CSV")
-   df = pd.read_csv(file_csv)
+   df = pd.read_csv(file_csv,delimiter=',')
    st.write(df)
 
    def analyze(score):
@@ -37,16 +36,15 @@ try:
 
    X_train, X_test, y_train,y_test = train_test_split(X,y, test_size=0.2,random_state=123)
 
-   modelnb = GaussianNB()
-   nbtrain = modelnb.fit(X_train, y_train)
-   nbtrain.class_count_
-   y_pred = nbtrain.predict(X_test)
+   #model
+   model = MultinomialNB()
+   model.fit(X_train, y_train)
+   pred = model.predict(X_test)
 
-   st.write(classification_report(y_test, y_pred))
-   #st.write('Accuracy : ',accuracy_score(y_test,pred))
-   #st.write('F1 Score : ',f1_score(y_test.astype(np.int16), pred, average='macro'))
-   #st.write('Precision : ',precision_score(y_test, pred, average='macro'))
-   #st.write('Recall : ',recall_score(y_test, pred, average='macro'))
-   #st.text('Model Report :\n'+classification_report(y_test,pred))
+   st.write('Accuracy : ',accuracy_score(y_test,pred))
+   st.write('F1 Score : ',f1_score(y_test.astype(np.int16), pred, average='macro'))
+   st.write('Precision : ',precision_score(y_test, pred, average='macro'))
+   st.write('Recall : ',recall_score(y_test, pred, average='macro'))
+   st.text('Model Report :\n'+classification_report(y_test,pred))
 except:
    print("error")
