208af25f705dc1f515be3db865cef73ceaac0362
bayuputpra <bayupp.bp@gmail.com>
2022-10-27 19:00:24 +0700

    first commit

--- pages/4_🎥_Video.py
+++ pages/4_🎥_Video.py
@@ -5,11 +5,12 @@ st.sidebar.success("Pilih Halaman Diatas")
 def main():
     st.title("Video")
 
-try:
-    searchVid = st.text_input("Masukan Link Video")
-    st.video("https://www.youtube.com/watch?v="+searchVid, format="video/mp4", start_time=0)
-except:
-    print("error")
-
 if __name__=='__main__':
     main()
+
+try:
+    searchVid = st.text_input("Masukan Link Video")
+    if searchVid != "null":
+        st.video("https://www.youtube.com/watch?v="+searchVid, format="video/mp4", start_time=0)
+except:
+    print("error")
