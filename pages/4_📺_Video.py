da0d0b0cbd4cb0bef671cd5ea3de2351ee21b425
bayuputpra <bayupp.bp@gmail.com>
2022-10-27 16:39:13 +0700

    first commit

--- pages/4_📺_Video.py
+++ pages/4_📺_Video.py
@@ -2,11 +2,5 @@ import streamlit as st
 
 st.sidebar.success("Pilih Halaman Diatas")
 
-def main():
-    st.title("Video")
-
-Vid=""
+Vid = st.title("Video")
 st.video("https://www.youtube.com/watch?v="+Vid, format="video/mp4", start_time=0)
-
-if __name__=='__main__':
-        main()
