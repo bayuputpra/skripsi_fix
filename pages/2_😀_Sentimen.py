103ef447ca9c9059a8d6152e019dac964f26c736
bayuputpra <bayupp.bp@gmail.com>
2022-10-28 16:33:05 +0700

    first commit

--- pages/2_😀_Sentimen.py
+++ pages/2_😀_Sentimen.py
@@ -22,7 +22,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 searchVid = st.text_input("Masukan Link Video")
 searchKom = st.text_input("Masukan Jumlah Komentar Yang Dicari")
 box = [['Name', 'Comment', 'Time', 'Likes', 'Reply Count']]
-data = youtube.commentThreads().list(part='snippet', videoId=searchVid, maxResults='100', textFormat="plainText").execute()
+data = youtube.commentThreads().list(part='snippet', videoId=searchVid, maxResults=searchKom, textFormat="plainText").execute()
 for i in data["items"]:
     name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
     comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
@@ -33,7 +33,7 @@ for i in data["items"]:
     box.append([name, comment, published_at, likes, replies])
 
 while ("nextPageToken" in data):
-    data = youtube.commentThreads().list(part='snippet', videoId=searchVid, pageToken=data["nextPageToken"],maxResults=searchKom, textFormat="plainText").execute()
+    data = youtube.commentThreads().list(part='snippet', videoId=searchVid, pageToken=data["nextPageToken"], textFormat="plainText").execute()
     for i in data["items"]:
         name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
         comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
