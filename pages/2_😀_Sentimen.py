bbb47c72f056204b4f4e0cc8c5495c34c6664c0e
bayuputpra <bayupp.bp@gmail.com>
2022-10-29 01:56:48 +0700

    first commit

--- pages/2_😀_Sentimen.py
+++ pages/2_😀_Sentimen.py
@@ -142,7 +142,6 @@ def main():
             file_csv=st.file_uploader("Unggah File CSV")
             dataset = pd.read_csv(file_csv)
             st.text("Dataset")
-            dataset=dataset.iloc[:,1:-1]
             dataset=dataset.drop(index=0, axis=0, inplace=False)
             st.write(dataset)
 
