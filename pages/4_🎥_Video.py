6ea9a9626db044b821a50d1fb0881519943f25d2
bayuputpra <bayupp.bp@gmail.com>
2022-10-29 19:52:04 +0700

    first commit

--- pages/4_🎥_Video.py
+++ pages/4_🎥_Video.py
@@ -5,7 +5,7 @@ st.sidebar.success("Pilih Halaman Diatas")
 st.title("Video")
 
 try:
-    searchVid = st.text_input("Masukan Link Video (https://www.youtube.com/watch?v="+"\u0332".join("C7Ly_HN-OCQ)"))
+    searchVid = st.text_input("Masukan Link Video (https://www.youtube.com/watch?v="+"\033[4m"+"C7Ly_HN-OCQ)"+"\033[0m")
     st.video("https://youtu.be/"+searchVid, format="video/mp4", start_time=0)
 except:
     print("error")
