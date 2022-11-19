ba54d543112fc627b773bd239b118db707021f53
bayuputpra <bayupp.bp@gmail.com>
2022-11-20 06:57:01 +0700

    first commit

--- 1_🏠_Beranda.py
+++ 1_🏠_Beranda.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
 	page_title="Skripsi",
 )
-st.title("Judul Skripsi")
+st.title("Analisis Sentimen Komentar Warganet Mengenai Video Musik Flying High Pada Youtube JKT48 Menggunakan Metode Naive Bayes")
 st.image("logo.png",width=300)
 st.sidebar.success("Pilih Halaman Diatas")
 hide_st_style="""
